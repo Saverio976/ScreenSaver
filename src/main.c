1806e7004e1cb310fcaa9f98ddae110074e934d1
saverio <xavier.mitault@epitech.eu>
2021-11-19 19:32:24 +0100

    [add] src/master_event.c src/main.c: new feature arrok key lef and right switch the screens

--- src/main.c
+++ src/main.c
@@ -17,8 +17,22 @@ static const struct screen_struct SCREENS[4] = {
     {&screen_spawn},
     {&screen_nico}
 };
+static const struct screen_explain EXPLAINS[4] = {
+    {&explain_circles},
+    {&explain_snow},
+    {&explain_spawn},
+    {&explain_nico}
+};
 
-static int print_help(char const *exe)
+static int print_bad_args(char const *exe)
+{
+    my_putstr(exe);
+    my_putstr(": bad arguments: 0 given but 1 is required\n");
+    my_putstr("retry with -h\n");
+    return (84);
+}
+
+static int print_h(char const *exe)
 {
     my_putstr("annimation rendering in a CSFML window.\n\n");
     my_putstr("USAGE\n\t");
@@ -35,26 +49,53 @@ static int print_help(char const *exe)
     return (0);
 }
 
+static int print_bad_id(char const *exe)
+{
+    my_putstr(exe);
+    my_putstr(": bad arguments: the given ID must be between 1 and ");
+    my_putnbr(ID_MAX);
+    my_putstr("\n");
+    return (84);
+}
+
+static int print_d(void)
+{
+    for (int i = 0; i < ID_MAX; i++) {
+        my_putnbr(i + 1);
+        my_putstr(": ");
+        EXPLAINS[i].fptr();
+        my_putstr("\n");
+    }
+    return (0);
+}
+
+int loop_until_close(int nb)
+{
+    int ret_code = SCREENS[nb - 1].fptr(1920, 1080);
+
+    while (ret_code == 2 || ret_code == 3) {
+        if (ret_code == 2)
+            nb = (nb - 1 == 0) ? ID_MAX : nb - 1;
+        if (ret_code == 3)
+            nb = (nb + 1 > ID_MAX) ? 1 : nb + 1;
+        ret_code = SCREENS[nb - 1].fptr(1920, 1080);
+    }
+    return (ret_code);
+}
+
 int main(int ac, char **av)
 {
     int nb;
 
-    if (ac < 2) {
-        my_putstr(av[0]);
-        my_putstr(": bad arguments: 0 given but 1 is required\n");
-        my_putstr("retry with -h\n");
-        return (84);
-    }
+    if (ac < 2)
+        return (print_bad_args(av[0]));
     if (av[1][0] == '-' && av[1][1] == 'h')
-        return (print_help(av[0]));
+        return (print_h(av[0]));
+    if (av[1][0] == '-' && av[1][1] == 'd')
+        return (print_d());
     nb = my_getnbr(av[1]);
-    if (nb <= 0 || nb > ID_MAX) {
-        my_putstr(av[0]);
-        my_putstr(": bad arguments: the given ID must be between 1 and ");
-        my_putnbr(ID_MAX);
-        my_putstr("\n");
-        return (84);
-    }
+    if (nb <= 0 || nb > ID_MAX)
+        return (print_bad_id(av[0]));
     srand(time(NULL));
-    return (SCREENS[nb - 1].fptr(1920, 1080));
+    return (loop_until_close(nb));
 }
---
+++ src/master_event.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** master arrow event in csfml and close event
+*/
+
+#include <SFML/Graphics.h>
+#include "graphics.h"
+
+static int master_sfevtkeypressed(sfEvent *event)
+{
+    int ret_code = 0;
+
+    if (event->key.code == sfKeyLeft)
+        ret_code = 2;
+    if (event->key.code == sfKeyRight)
+        ret_code = 3;
+    return (ret_code);
+}
+
+int master_event(context_t *ctx)
+{
+    sfEvent event;
+    int ret_code = 1;
+
+    while (ret_code == 1 && sfRenderWindow_pollEvent(ctx->win, &event)) {
+        if (event.type == sfEvtClosed)
+            ret_code = 0;
+        if (event.type == sfEvtKeyPressed)
+            ret_code = master_sfevtkeypressed(&event);
+    }
+    if (ret_code != 1)
+        sfRenderWindow_close(ctx->win);
+    return (ret_code);
+}
