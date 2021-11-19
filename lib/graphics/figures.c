7be6cba1fe141736db4eb9d8cad25996c95e687c
saverio <xavier.mitault@epitech.eu>
2021-11-19 15:41:10 +0100

    [fix] lib/graphics/figures.c: better draw figures

--- lib/graphics/figures.c
+++ lib/graphics/figures.c
@@ -12,9 +12,14 @@ static void do_condition_line(framebuffer_t *buffer, sfVector2u pos,
                             int condition[2], sfColor color)
 {
     unsigned int calc1 = condition[0] * pos.x + condition[1];
-    unsigned int calc2 = (pos.y - condition[1]) / condition[0];
+    unsigned int calc2;
 
-    if (pos.y == calc1 - 2 && calc1 + 2 >= pos.x)
+    if (pos.x <= 0 || pos.y <= 0 || pos.x > buffer->w || pos.y > buffer->h)
+        return;
+    if (condition[0] == 0)
+        condition[0] = 1;
+    calc2 = (pos.y - condition[1]) / condition[0];
+    if (pos.y == calc1 - 5 && calc1 + 5 >= pos.x)
         my_putpixel(buffer, pos.x, pos.y, color);
     if (pos.x >= calc2 - 2 && calc2 + 2 >= pos.x)
         my_putpixel(buffer, pos.x, pos.y, color);
@@ -25,11 +30,15 @@ void draw_line(framebuffer_t *buffer, sfVector2i point_a, sfVector2i point_b,
 {
     sfVector2u position = {0, 0};
     int condition[2] = {0};
+    unsigned int start_x = (point_a.x > point_b.x) ? point_b.x : point_a.x;
+    unsigned int start_y = (point_a.y > point_b.y) ? point_b.y : point_a.y;
+    unsigned int end_x = (point_a.x > point_b.x) ? point_a.x : point_b.x;
+    unsigned int end_y = (point_a.y > point_b.y) ? point_a.y : point_b.y;
 
     condition[0] = (point_a.y - point_b.y) / (point_a.x - point_b.x);
     condition[1] = point_a.y - condition[0] * point_a.x;
-    for (unsigned int y = 0; y < buffer->h; y++) {
-        for (unsigned int x = 0; x < buffer->w; x++) {
+    for (unsigned int y = start_y; y < end_y; y++) {
+        for (unsigned int x = start_x; x < end_x; x++) {
             position.x = x;
             position.y = y;
             do_condition_line(buffer, position, condition, color);
