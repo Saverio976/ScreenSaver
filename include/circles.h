7804cb4bb32fd46c520ee74d7e87661597483e1f
saverio <xavier.mitault@epitech.eu>
2021-11-19 19:30:10 +0100

    [add] include/circles.h: prototype of function

--- include/circles.h
+++ include/circles.h
@@ -21,6 +21,7 @@ struct circles_struct {
 };
 typedef struct circles_struct circles_t;
 
+void destroy_circles(context_t *, circles_t **, sfClock *, sfClock *);
 void draw_circles(framebuffer_t *, circles_t **);
 void create_2_clock(sfClock **, sfClock **);
 void destroy_2_clock(sfClock *, sfClock *);
