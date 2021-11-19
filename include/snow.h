1cb96adf858415f7a12dfa29ed00f5c2e844d23e
saverio <xavier.mitault@epitech.eu>
2021-11-19 15:34:00 +0100

    [add] include/snow.h: header for the snow screen

---
+++ include/snow.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** header for lines screen
+*/
+
+#ifndef LINES_H_
+    #define LINES_H_
+
+    #include <SFML/Graphics.h>
+
+struct lines_struct {
+    sfVector2i *start;
+    sfVector2i *end;
+    sfColor color;
+    int moove;
+    int grow;
+};
+typedef struct lines_struct lines_t;
+
+#endif
