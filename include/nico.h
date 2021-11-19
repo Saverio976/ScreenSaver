a8d47ab62f194cedb9067c9395397ae2913f3d27
saverio <xavier.mitault@epitech.eu>
2021-11-19 15:33:40 +0100

    [add] include/nico.h: header for the nicolas screen

---
+++ include/nico.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** settings for nico screen
+*/
+
+#ifndef NICO_H_
+    #define NICO_H_
+
+    #define IMAGE_DVD_NICO "assets/nico.bmp"
+
+struct image_struct {
+    sfVector2i pos;
+    sfVector2i moove;
+};
+typedef struct image_struct image_t;
+
+#endif
