7a805ec645dda46a725537a3305e374615132dae
saverio <xavier.mitault@epitech.eu>
2021-11-19 19:35:20 +0100

    [add] include/myscreensaver.h: possibility to change the bg color

--- include/myscreensaver.h
+++ include/myscreensaver.h
@@ -9,15 +9,27 @@
     #define MYSCREENSAVER_H_
 
     #include <SFML/Graphics.h>
-    #include <graphics.h>
+    #include "graphics.h"
+
+    #define BG_COLOR sfRed
 
 struct screen_struct {
     int (*fptr)(unsigned int, unsigned int);
 };
+struct screen_explain {
+    void (*fptr)(void);
+};
+
+int master_event(context_t *);
 
 int screen_circles(unsigned int, unsigned int);
 int screen_snow(unsigned int, unsigned int);
 int screen_spawn(unsigned int, unsigned int);
 int screen_nico(unsigned int, unsigned int);
+
+void explain_circles(void);
+void explain_snow(void);
+void explain_spawn(void);
+void explain_nico(void);
 
 #endif
