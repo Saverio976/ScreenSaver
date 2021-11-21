511219ed1226c54ccc840be24578cf12392b69c8
saverio <xavier.mitault@epitech.eu>
2021-11-21 18:58:48 +0100

    [add] src/bubulles/circles_t_iscolide.c: functions to check colisions between circles or out the screen

---
+++ src/bubulles/circles_t_iscolide.c
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** check if a circles_t collide over in a list
+*/
+
+#include <math.h>
+#include "graphics.h"
+#include "circles.h"
+
+int circles_t_iscolide(circles_t **circles, int index)
+{
+    int is_colide = -1;
+    int dist;
+    int rad;
+
+    for (int i = 0; is_colide == -1 && circles[i] != NULL; i++) {
+        if (i == index)
+            continue;
+        dist = pow(circles[i]->pos->x - circles[index]->pos->x, 2) +
+            pow(circles[i]->pos->y - circles[index]->pos->y, 2);
+        rad = pow(circles[i]->radius + circles[index]->radius, 2) +
+            circles[index]->radius * 2;
+        if (dist <= rad)
+            is_colide = i;
+    }
+    return (is_colide);
+}
+
+int circles_t_isout_x(circles_t *circle, framebuffer_t *buffer)
+{
+    if (circle->pos->x - circle->radius <= 0 + circle->radius / 2)
+        return (1);
+    if (circle->pos->x + circle->radius >=
+            (int) buffer->w - circle->radius / 2)
+        return (1);
+    return (0);
+}
+
+int circles_t_isout_y(circles_t *circle, framebuffer_t *buffer)
+{
+    if (circle->pos->y - circle->radius <= 0 + circle->radius / 2)
+        return (1);
+    if (circle->pos->y + circle->radius >=
+            (int) buffer->h - circle->radius / 2)
+        return (1);
+    return (0);
+}
