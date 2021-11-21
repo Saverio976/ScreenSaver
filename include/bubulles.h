9a4adeba3c52109ca99d3f423bcf9cc7c4a67b41
saverio <xavier.mitault@epitech.eu>
2021-11-21 18:54:49 +0100

    [add] include/bubulles.h: functions prototype for the 5th screen

---
+++ include/bubulles.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** header for bubulles screen
+*/
+
+#ifndef BUBULLES_H_
+    #define BUBULLES_H_
+
+    #include "circles.h"
+    #include "graphics.h"
+    #include <SFML/Graphics.h>
+
+int circles_t_iscolide(circles_t **, int);
+int circles_t_isout_x(circles_t *, framebuffer_t *);
+int circles_t_isout_y(circles_t *, framebuffer_t *);
+void draw_plain_circle(framebuffer_t *, sfVector2i, int, sfColor);
+void draw_all_circles(context_t *, circles_t **, sfClock *);
+
+#endif
