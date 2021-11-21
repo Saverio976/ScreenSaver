d02845fbd4db38e7ee7b4bf348a7a8a0fb4ba739
saverio <xavier.mitault@epitech.eu>
2021-11-21 19:00:12 +0100

    [add] src/bubulles/explain.c: add explanation for the 5th screen

---
+++ src/bubulles/explain.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** explain the bubulles screen
+*/
+
+#include "my.h"
+
+void explain_bubulles(void)
+{
+    my_putstr("windows like bubulles bounce");
+}
