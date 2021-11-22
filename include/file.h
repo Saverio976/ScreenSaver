fe6d51aaa207f7744e72ff97338da566cd7a766f
saverio <xavier.mitault@epitech.eu>
2021-11-22 22:36:20 +0100

    [remove] include/file.h: remove unused functions prototype (all functions that use stat() a forbidden function)

--- include/file.h
+++ include/file.h
@@ -8,10 +8,6 @@
 #ifndef FILE_H_
     #define FILE_H_
 
-int has_file_perm(char const *, char);
-int is_file(char const *, char);
 int open_ronly(char const *);
-int file_size(char const *);
-char *fs_get_content(char const *);
 
 #endif
