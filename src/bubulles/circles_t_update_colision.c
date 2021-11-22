7669be21824a61bc04952bf107ce815d1a478701
saverio <xavier.mitault@epitech.eu>
2021-11-22 14:10:38 +0100

    [add] src/bubulles/circles_t_update_colision.c: better moovement for colision

---
+++ src/bubulles/circles_t_update_colision.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** update two cirles that collide
+*/
+
+#include "bubulles.h"
+#include "circles.h"
+
+void circles_t_remove_from_out(circles_t *c, framebuffer_t *buf)
+{
+    int is_x = 0;
+    int is_y = 0;
+
+    while (circles_t_isout_x(c, buf) || circles_t_isout_y(c, buf)) {
+        if (is_x == 0 && circles_t_isout_x(c, buf)) {
+            c->moove_x *= -1;
+            is_x = 1;
+        }
+        if (is_y == 0 && circles_t_isout_y(c, buf)) {
+            c->moove_y *= -1;
+            is_y = 1;
+        }
+        c->pos->x += c->moove_x;
+        c->pos->y += c->moove_y;
+   }
+}
+
+static void mult_minus_all(circles_t *c1, circles_t *c2)
+{
+    c1->moove_y *= -1;
+    c1->moove_x *= -1;
+    c2->moove_y *= -1;
+    c2->moove_x *= -1;
+}
+
+static void collide_same_sens(circles_t *c1, circles_t *c2)
+{
+    if (c1->pos->y > c2->pos->y)
+        c2->moove_x *= -1;
+    else
+        c1->moove_x *= -1;
+    if (c1->pos->x > c2->pos->x)
+        c2->moove_y *= -1;
+    else
+        c1->moove_y *= -1;
+}
+
+void circles_t_update_colision(circles_t *c1, circles_t *c2)
+{
+    if (c1->moove_y * c2->moove_y > 0 && c1->moove_x * c2->moove_x > 0) {
+        collide_same_sens(c1, c2);
+        return;
+    }
+    if (c1->moove_y * c2->moove_y > 0) {
+        c1->moove_x *= -1;
+        c2->moove_x *= -1;
+    } else if (c1->moove_x * c2->moove_x > 0) {
+        c1->moove_y *= -1;
+        c2->moove_y *= -1;
+    } else
+        mult_minus_all(c1, c2);
+    c1->pos->x += c1->moove_x * 2;
+    c1->pos->y += c1->moove_y * 2;
+    c2->pos->x += c2->moove_x * 2;
+    c2->pos->y += c2->moove_y * 2;
+
+}
