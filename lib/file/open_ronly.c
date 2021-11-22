7af35df60a40eb1742353de88d94df0fa1bdc9f4
saverio <xavier.mitault@epitech.eu>
2021-11-22 22:40:30 +0100

    [update] lib/file/open_ronly.c: just a wraper of open without call stat to check perms or is readable

--- lib/file/open_ronly.c
+++ lib/file/open_ronly.c
@@ -9,15 +9,10 @@
 #include "file.h"
 
 // open read only
-// return -1 if path is not a file
-// return -2 if exe has not read permissions
-// above or equal 0 for sucess
 
 int open_ronly(char const *path)
 {
-    if (!is_file(path, 'f'))
-        return (-1);
-    if (!has_file_perm(path, 'r'))
-        return (-2);
-    return (open(path, O_RDONLY));
+    int fd = open(path, O_RDONLY);
+
+    return (fd);
 }
