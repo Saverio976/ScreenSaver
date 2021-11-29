fc77f4a0481ad74a78585d1cf84dc1a77b162c9f
saverio <xavier.mitault@epitech.eu>
2021-11-29 23:55:58 +0100

    [add] include/myscreensaver.h: add function prototype of the screen square divide

--- include/myscreensaver.h
+++ include/myscreensaver.h
@@ -31,6 +31,7 @@ int screen_spawn(unsigned int, unsigned int);
 int screen_nico(unsigned int, unsigned int);
 int screen_bubulles(unsigned int, unsigned int);
 int screen_bg_fade(unsigned int, unsigned int);
+int screen_quare_rec(unsigned int, unsigned int);
 
 void explain_circles(void);
 void explain_snow(void);
@@ -38,6 +39,7 @@ void explain_spawn(void);
 void explain_nico(void);
 void explain_bubulles(void);
 void explain_bg_fade(void);
+void explain_square_rec(void);
 
 int print_bad_args(char const *);
 int print_h(char const *, int);
