d5809ccfc79b59c8009e1c1abe92bf5f64fec532
saverio <xavier.mitault@epitech.eu>
2021-11-29 23:56:47 +0100

    [add] include/square_rec.h: add the header for the square divide part

---
+++ include/square_rec.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** the header for the squrea rec screen
+*/
+
+#ifndef SQUARE_REC_H_
+    #define SQUARE_REC_H_
+
+    #define PADDING 3
+
+struct rectangle_struct {
+    unsigned int x;
+    unsigned int y;
+    int size_x;
+    int size_y;
+};
+typedef struct rectangle_struct rectangle_t;
+
+#endif
