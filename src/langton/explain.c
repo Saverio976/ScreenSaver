ff45dba1f6e7b53eb689ca0d7b3ca77d8ac8521e
saverio <xavier.mitault@epitech.eu>
2021-12-01 21:22:51 +0100

    [add] src/langton/explain.c: explanation of the langton ant screen

---
+++ src/langton/explain.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** explain langton screensaver
+*/
+
+#include "my.h"
+
+void explain_langton(void)
+{
+    my_putstr("the langton ant, an ant tha moove right or left depends on ");
+    my_putstr("the situation");
+}
