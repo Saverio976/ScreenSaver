fc1293d0e258d8a429766d5b135d7fa982636e1a
saverio <xavier.mitault@epitech.eu>
2021-12-02 15:10:09 +0100

    [add] src/langton/table_t_create.c: add field for the langton struct board

--- src/langton/table_t_create.c
+++ src/langton/table_t_create.c
@@ -17,15 +17,17 @@ table_t *table_t_create(void)
         return (NULL);
     table->ant_x = NUMBER_COL_CASE / 2;
     table->ant_y = NUMBER_LINE_CASE / 2;
+    table->size_y = NUMBER_LINE_CASE;
+    table->size_x = NUMBER_COL_CASE;
     table->ant_angle = get_random_between(0, 3);
-    table->table = malloc(sizeof(char *) * NUMBER_LINE_CASE);
+    table->table = malloc(sizeof(char *) * table->size_y);
     if (!table->table)
         return (NULL);
-    for (int i = 0; i < NUMBER_LINE_CASE; i++) {
-        table->table[i] = malloc(sizeof(char) * NUMBER_COL_CASE);
+    for (int i = 0; i < table->size_y; i++) {
+        table->table[i] = malloc(sizeof(char) * table->size_x);
         if (!table->table[i])
             return (NULL);
-        for (int j = 0; j < NUMBER_COL_CASE; j++)
+        for (int j = 0; j < table->size_x; j++)
             table->table[i][j] = get_random_between(0, 1);
     }
     return (table);
