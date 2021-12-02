f1a6e86790998d8d16888071d2e6aff574212d5a
saverio <xavier.mitault@epitech.eu>
2021-12-02 20:01:43 +0100

    [add] src/barnsley_fern/explain.c: add explanation of the barnsley screen

---
+++ src/barnsley_fern/explain.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** explain the barnsley fern screen
+*/
+
+#include "my.h"
+
+void explain_barnsleyfern(void)
+{
+    my_putstr("the barnsley fern 'fractal'");
+}
