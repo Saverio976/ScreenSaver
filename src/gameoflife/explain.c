a765dd99696501279add51ce5223e7bd57e7bf0d
saverio <xavier.mitault@epitech.eu>
2021-12-02 12:22:17 +0100

    [add] src/gameoflife/explain.c: add explantion for the screen gameoflife

---
+++ src/gameoflife/explain.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** explain the gameoflif screen
+*/
+
+#include "my.h"
+
+void explain_gameoflife(void)
+{
+    my_putstr("a conway game of life screen");
+}
