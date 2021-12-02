c60fc13cd7d1802c24dffa2ac889093c0b76b46e
saverio <xavier.mitault@epitech.eu>
2021-12-02 12:21:23 +0100

    [add] include/gameoflife.h: the screen header

---
+++ include/gameoflife.h
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** header for the screen gameoflife
+*/
+
+#ifndef GAMEOFLIFE_H_
+    #define GAMEOFLIFE_H_
+
+    #include <SFML/Graphics.h>
+    #include "graphics.h"
+
+    #define NUMBER_COL_CASE 30
+    #define NUMBER_LINE_CASE 30
+    #define DEFT_COLOR_1 sfBlack
+    #define DEFT_COLOR_2 sfWhite
+
+struct gameoflife_struct {
+    char **board;
+    int size_y;
+    int size_x;
+};
+typedef struct gameoflife_struct gameoflife_t;
+
+void gameoflife_t_check_three_next(gameoflife_t *, int, int);
+gameoflife_t *gameoflife_t_create(void);
+void gameoflife_t_destroy(gameoflife_t *);
+void gameoflife_t_to_buffer(gameoflife_t *, framebuffer_t *);
+
+#endif
