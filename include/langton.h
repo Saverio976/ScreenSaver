892e4b4e8c67a51caa9af1f965cefe8bd5039080
saverio <xavier.mitault@epitech.eu>
2021-12-02 15:09:16 +0100

    [update] include/langton.h: another resolution for each cell of langton

--- include/langton.h
+++ include/langton.h
@@ -13,13 +13,15 @@
 
     #define COLOR_TABLE_1 sfCyan
     #define COLOR_TABLE_2 sfRed
-    #define NUMBER_COL_CASE 50
-    #define NUMBER_LINE_CASE 25
+    #define NUMBER_COL_CASE 10
+    #define NUMBER_LINE_CASE 6
 
 struct table_struct {
     char **table;
     int ant_x;
     int ant_y;
+    int size_y;
+    int size_x;
     int ant_angle;
 };
 typedef struct table_struct table_t;
