5a6420f5084ad26342cddb2a4817bd342238ec19
saverio <xavier.mitault@epitech.eu>
2021-12-02 21:23:19 +0100

    [add] src/fract_square/explain.c: explanation for the square fact screen

---
+++ src/fract_square/explain.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** MYSCREENSAVER
+** File description:
+** explain the square fact screen
+*/
+
+#include "my.h"
+
+void explain_fractsquare(void)
+{
+    my_putstr("a square that repeat itself on its border");
+}
