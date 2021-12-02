988ffc9d3a56a257df01d82e7f42bb98af01b9d1
saverio <xavier.mitault@epitech.eu>
2021-12-02 15:10:26 +0100

    [add] src/langton/table_t_to_framebuffer.c: better render of a cell

--- src/langton/table_t_to_framebuffer.c
+++ src/langton/table_t_to_framebuffer.c
@@ -9,38 +9,39 @@
 #include "graphics.h"
 #include "langton.h"
 
-static void put_square(framebuffer_t *buffer, sfVector2u pos, sfColor color)
+static void put_square(framebuffer_t *buffer, sfVector2u pos, sfColor color,
+        table_t *table)
 {
-    int pad_x = buffer->w / NUMBER_COL_CASE;
-    int pad_y = buffer->h / NUMBER_LINE_CASE;
+    int pad_x = buffer->w / table->size_x;
+    int pad_y = buffer->h / table->size_y;
 
-    for (unsigned int i = pos.y; i < pos.y + pad_y; i++)
-        for (unsigned int j = pos.x; j < pos.x + pad_x; j++)
+    for (unsigned int i = pos.y; i < pad_y + pos.y; i++)
+        for (unsigned int j = pos.x; j < pad_x + pos.x; j++)
             my_putpixel(buffer, j, i, color);
 }
 
-static void put_square_color(table_t *table, int y, int x,
+static void put_square_from(table_t *table, int y, int x,
         framebuffer_t *buffer)
 {
-    sfVector2u pos = {x * buffer->w / NUMBER_COL_CASE,
-        y * buffer->h / NUMBER_LINE_CASE};
+    sfVector2u pos = {x * buffer->w / table->size_x,
+                        y * buffer->h / table->size_y};
 
     if (table->table[y][x] == 0)
-        put_square(buffer, pos, COLOR_TABLE_1);
+        put_square(buffer, pos, COLOR_TABLE_2, table);
     else
-        put_square(buffer, pos, COLOR_TABLE_2);
+        put_square(buffer, pos, COLOR_TABLE_1, table);
 }
 
 void table_t_to_framebuffer(table_t *table, framebuffer_t *buffer)
 {
-    int pad_x = buffer->w / NUMBER_COL_CASE;
-    int pad_y = buffer->h / NUMBER_LINE_CASE;
+    int pad_x = buffer->w / table->size_x;
+    int pad_y = buffer->h / table->size_y;
     sfVector2i pos = {table->ant_x * pad_x + pad_x / 2,
         table->ant_y * pad_y + pad_y / 2};
 
-    for (int i = 0; i < NUMBER_LINE_CASE; i++) {
-        for (int j = 0; j < NUMBER_COL_CASE; j++) {
-            put_square_color(table, i, j, buffer);
+    for (int i = 0; i < table->size_y; i++) {
+        for (int j = 0; j < table->size_x; j++) {
+            put_square_from(table, i, j, buffer);
         }
     }
     draw_circle(buffer, pos, pad_y / 2, sfBlack);
